7f7f97a9e6ede36c8049c37a813e679ff2244754
wgarciab <55595765+wgarciab@users.noreply.github.com>
2022-10-03 19:47:23 -0500

    Added solution to questions 6 and 7

--- pregunta_07.sql
+++ pregunta_07.sql
@@ -43,3 +43,14 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --
+SELECT
+    *
+FROM
+    tbl1
+WHERE
+    K0 != 'A'
+    AND K0 != 'B'
+    AND c13 != 200
+    AND c13 != 900
+ORDER BY
+    c14 ASC;
